126b1a4300ebf321321cf2a7222453596bd86943
Paul Mitchum <paul@mile23.com>
2013-03-08 12:25:04 -0800

    some test infrastructure

---
+++ symfony/src/VirtualPersistAPI/VirtualPersistBundle/Tests/Entity/UserTest.php
@@ -0,0 +1,13 @@
+<?php
+namespace VirtualPersistAPI\VirtualPersistBundle\Entity;
+
+use VirtualPersistAPI\VirtualPersistBundle\Entity\User;
+
+class UserEntity extends \PHPUnit_Framework_TestCase
+{
+
+  public function testThing() {
+    $this->assertTrue(TRUE);
+  }
+
+}
