60ac85d86de2126f7ca0ca4d39661c0f2c91c8ac
Paul Mitchum <paul@mile23.com>
2013-03-08 17:22:06 -0800

    some routing

--- symfony/src/VirtualPersistAPI/VirtualPersistBundle/Controller/DefaultController.php
+++ symfony/src/VirtualPersistAPI/VirtualPersistBundle/Controller/DefaultController.php
@@ -3,17 +3,32 @@
 namespace VirtualPersistAPI\VirtualPersistBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
+/**
+ * Our prefix:
+ * @Route("/api")
+ */
 class DefaultController extends Controller
 {
     /**
-     * @Route("/hello/{name}")
-     * @Template()
+     * @Route("/{uuid}/{category}/{key}")
      */
-    public function indexAction($name)
+    public function getAction($uuid, $category, $key)
     {
-        return array('name' => $name);
+      $tellme = 'uuid: ' . $uuid . ' category: ' . $category . ' key: ' . $key;
+      $response = new Response();
+//        $tellme,
+//      200,
+        //array('content-type' => 'text/plain')
+  //    );
+      $response->setContent($tellme);
+      $request = Request::createFromGlobals();
+      $response->prepare($request);
+//      $response->send();
+      return $response;
     }
 }
