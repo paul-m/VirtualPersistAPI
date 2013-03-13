372c9ae94d171c8667bfa273c56cd27ae04c1790
Paul Mitchum <paul@mile23.com>
2013-03-12 20:38:07 -0700

    feh

--- symfony/app/config/parameters.pagoda.php
+++ symfony/app/config/parameters.pagoda.php
@@ -8,7 +8,7 @@
     database_password: %database.password%
 */
 
-$container->setParameter('database.host', $_SERVER['DB1_HOST']);
+$container->setParameter('database.host', $_SERVER['SYMFONY_HOST']);
 $container->setParameter('database.port', $_SERVER['DB1_PORT']);
 $container->setParameter('database.name', $_SERVER['DB1_NAME']);
 $container->setParameter('database.user', $_SERVER['DB1_USER']);
