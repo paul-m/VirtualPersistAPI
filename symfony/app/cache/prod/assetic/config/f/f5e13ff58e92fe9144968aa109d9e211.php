f85cba3ad37955db2f461f638db2386c8ea53c8b
Paul Mitchum <paul@mile23.com>
2013-09-15 11:06:28 -0700

    hateful cache

---
+++ symfony/app/cache/prod/assetic/config/f/f5e13ff58e92fe9144968aa109d9e211.php
@@ -0,0 +1,89 @@
+<?php
+
+// VirtualPersistBundle:Default:useage.html.twig
+return array (
+  'ff5fb4a' => 
+  array (
+    0 => 
+    array (
+      0 => '@VirtualPersistBundle/Resources/public/js/traffic/*',
+    ),
+    1 => 
+    array (
+    ),
+    2 => 
+    array (
+      'output' => 'js/ff5fb4a.js',
+      'name' => 'ff5fb4a',
+      'debug' => NULL,
+      'combine' => NULL,
+      'vars' => 
+      array (
+      ),
+    ),
+  ),
+  '4d7307e' => 
+  array (
+    0 => 
+    array (
+      0 => '@VirtualPersistBundle/Resources/public/js/jqplot/jquery.jqplot.min.js',
+      1 => '@VirtualPersistBundle/Resources/public/js/jqplot/plugins/jqplot.dateAxisRenderer.min.js',
+      2 => '@VirtualPersistBundle/Resources/public/js/primuse/*',
+    ),
+    1 => 
+    array (
+    ),
+    2 => 
+    array (
+      'output' => 'js/compiled/traffic.js',
+      'name' => '4d7307e',
+      'debug' => NULL,
+      'combine' => NULL,
+      'vars' => 
+      array (
+      ),
+    ),
+  ),
+  'f461006' => 
+  array (
+    0 => 
+    array (
+      0 => '@VirtualPersistBundle/Resources/public/js/jqplot/jquery.jqplot.min.js',
+      1 => '@VirtualPersistBundle/Resources/public/js/jqplot/plugins/jqplot.dateAxisRenderer.min.js',
+      2 => '@VirtualPersistBundle/Resources/public/js/traffic/*',
+    ),
+    1 => 
+    array (
+    ),
+    2 => 
+    array (
+      'output' => 'js/compiled/traffic.js',
+      'name' => 'f461006',
+      'debug' => NULL,
+      'combine' => NULL,
+      'vars' => 
+      array (
+      ),
+    ),
+  ),
+  'a13db7d' => 
+  array (
+    0 => 
+    array (
+      0 => '@VirtualPersistBundle/Resources/public/js/*',
+    ),
+    1 => 
+    array (
+    ),
+    2 => 
+    array (
+      'output' => 'js/a13db7d.js',
+      'name' => 'a13db7d',
+      'debug' => NULL,
+      'combine' => NULL,
+      'vars' => 
+      array (
+      ),
+    ),
+  ),
+);
