503ec9ec71079ff56da16bae2a08b2bc3f4384a6
Paul Mitchum <paul@mile23.com>
2015-03-01 13:50:38 -0800

    more pagodabox env madness

---
+++ app/config/parametersPagodabox.php
@@ -0,0 +1,10 @@
+<?php
+
+$container->setParameter('database_driver', 'pdo_mysql');
+if(@$_SERVER['DATABASE1_HOST']) {
+    $container->setParameter('database_host', $_SERVER['DATABASE1_HOST']);
+    $container->setParameter('database_port', $_SERVER['DATABASE1_PORT']);
+    $container->setParameter('database_name', $_SERVER['DATABASE1_NAME']);
+    $container->setParameter('database_user', $_SERVER['DATABASE1_USER']);
+    $container->setParameter('database_password', $_SERVER['DATABASE1_PASS']);
+}
